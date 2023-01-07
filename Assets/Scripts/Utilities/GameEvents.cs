5b072ec462ecbc647449cfcfe858083a374d392a
Matthew Caldwell <matthew@matthew-caldwell.com>
2023-01-06 21:52:37 -0600

    Add game events singleton

---
+++ Assets/Scripts/Utilities/GameEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BoroGameDev.Utilities {
+
+	public class GameEvents {
+		#region Singleton
+		private static GameEvents instance = null;
+		private static readonly object padlock = new object();
+
+		GameEvents() { }
+
+		public static GameEvents Instance {
+			get {
+				lock (padlock) {
+					if (instance == null) {
+						instance = new GameEvents();
+					}
+					return instance;
+				}
+			}
+		}
+        #endregion
+
+        /**
+		 * Game Event Example:
+		 *		public event Action<type> onEventHappened;
+		 *		public void EventHappened(type param) {
+		 *			if (onEventHappened != null) {
+		 *				onEventHappened(param);
+		 *			}
+		 *		}
+		 *
+		 * To trigger an event:
+		 *		GameEvents.Instance.EventHappened(param);
+		 *
+		 * To subscribe to an event:
+		 *		void Awake() {
+		 *			GameEvents.Instance.onEventHappened += this.EventListener;
+		 *		}
+		 *	Unsubscribe to avoid memory leaks:
+		 *		void OnDestroy() {
+		 *			GameEvents.Instance.onEventHappened -= this.EventListener;
+		 *		}
+		 *		void EventListener(type param) {...}
+		 **/
+    }
+}
